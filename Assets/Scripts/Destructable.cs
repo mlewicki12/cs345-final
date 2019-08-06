5defc3b5a5096accad7d56993ff36ec4c49659ef
Maciej Lewicki <lewickima@drexel.edu>
2019-08-06 13:25:33 -0400

    destructable blocks

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject Fill;
+    public GameObject Parent;
+    public Material Colour;
+
+    // hardcoding it for now
+    public float XOffset;
+    public float YOffset;
+    public float ZOffset;
+
+    public float MaxSpeed;
+    public float FillSize;
+
+    public int Health;
+    
+    public float MinDeathTime;
+    public float MaxDeathTime;
+
+    private bool _replaced;
+
+    private Vector3 _size;
+    private Vector3 _newsize;
+    private Rigidbody _rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _replaced = false;
+
+        _size = GetComponent<Collider>().bounds.size;
+        _newsize = Fill.GetComponent<Collider>().bounds.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = new Vector3(XOffset, YOffset, ZOffset);
+        Health -= 1;
+
+        if (!_replaced && Health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log(_newsize);
+            
+            // beautiful triple for loop here
+            for (float i = 0; i < _size.x; i += FillSize)
+            {
+                for (float j = 0; j < _size.y; j += FillSize)
+                {
+                    for (float k = 0; k < _size.z; k += FillSize)
+                    {
+                        // doesn't spawn in the same position
+                        var obj = Instantiate(Fill, transform.position + offset + new Vector3(i, j, k), Quaternion.identity);
+                        obj.GetComponent<Renderer>().material = Colour;
+                        obj.transform.parent = Parent.transform;
+
+                        Vector3 vel = new Vector3(Random.Range(-1 * MaxSpeed, MaxSpeed), 
+                                                  Random.Range(0, MaxSpeed),
+                                                  Random.Range(-1 * MaxSpeed, MaxSpeed));
+
+                        obj.GetComponent<Rigidbody>().velocity = vel;
+                        Destroy(obj, Random.Range(MinDeathTime, MaxDeathTime));
+                    }
+                }
+            }
+
+            _replaced = true;
+        }
+    }
+}
