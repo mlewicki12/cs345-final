4231230da2e23476c319312e3b2f0f2b4b9a245a
Maciej Lewicki <lewickima@drexel.edu>
2019-08-06 13:42:06 -0400

    added player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * Speed; // AD
+        float moveZ = Input.GetAxis("Vertical") * Speed; // WS
+
+        moveX *= Time.deltaTime;
+        moveZ *= Time.deltaTime;
+
+        transform.Translate(-1 * moveX, 0.0f, -1 * moveZ);
+    }
+}
