95595f306b7e8bbcfea66fca1dc13d8c5f56a33c
Maciej Lewicki <lewickima@drexel.edu>
2019-08-22 12:03:11 -0400

    working on player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,56 +6,103 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public enum PlayerClass {
-        MAGE
+        Mage
     }
 
-    public float Speed;
+    public float Acceleration;
     public PlayerClass Class;
     public GameObject FireSource;
+    public Vector3 Decceleration;
+    public Vector3 MaxSpeed;
     public Vector3 SpellSpeed;
 
     public float FireTime;
     public float ProjectileLifeTime;
     
-    private float timeToFire;
-    private float cameraDiff;
-    private Vector3 worldPos;
+    private float _timeToFire;
+    private float _cameraDiff;
+    private Vector3 _worldPos;
+    private Vector3 _speed;
+    private Rigidbody _rigidbody;
 
     public GameObject MageSpell; // there's gotta be a better way to store spells
     // actually, GameObject[], allowing for players to switch spells
 
     void Start()
     {
-        timeToFire = 0f;
+        _timeToFire = 0f;
+        _speed = new Vector3(0, 0, 0);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    Vector3 Min(Vector3 first, Vector3 second)
+    {
+        float x, z;
+
+        if (Math.Abs(second.x) > first.x)
+        {
+            x = first.x * Math.Sign(second.x);
+        }
+        else x = second.x;
+
+        if (Math.Abs(second.z) > first.z)
+        {
+            z = first.z * Math.Sign(second.z);
+        }
+        else z = second.z;
+
+        return new Vector3(x, 0, z);
+    }
+
+    Vector3 Deccel(Vector3 inp, Vector3 speed)
+    {
+        speed = Decceleration * Time.deltaTime;
+        
+        if (inp.x < 0)
+        {
+            inp.x = Math.Min(inp.x + speed.x, 0);
+        }
+        else inp.x = Math.Max(inp.x - speed.x, 0);
+
+        if (inp.z < 0)
+        {
+            inp.z = Math.Min(inp.z + speed.z, 0);
+        }
+        else inp.z = Math.Max(inp.z - speed.z, 0);
+
+        return inp;
     }
     
     // Update is called once per frame
     void Update()
     {
-        timeToFire = Math.Max(0f, timeToFire - Time.deltaTime);
-        cameraDiff = Camera.main.transform.position.z - transform.position.z;
+        _timeToFire = Math.Max(0f, _timeToFire - Time.deltaTime);
+        _cameraDiff = Camera.main.transform.position.z - transform.position.z;
         
         FollowMouse();
         
-        float moveX = Input.GetAxis("Horizontal") * Speed; // AD
-        float moveZ = Input.GetAxis("Vertical") * Speed; // WS
+        float moveX = Input.GetAxis("Horizontal") * Acceleration; // AD
+        float moveZ = Input.GetAxis("Vertical") * Acceleration; // WS
 
         moveX *= Time.deltaTime;
         moveZ *= Time.deltaTime;
 
-        transform.Translate(moveX, 0.0f, moveZ);
+        _speed += new Vector3(-moveX, 0, moveZ);
+        _speed = Deccel(_speed, Decceleration);
+        
+        _rigidbody.velocity = Min(MaxSpeed, _speed);
 
-        if (Input.GetMouseButton(0) && timeToFire <= 0f)
+        if (Input.GetMouseButton(0) && _timeToFire <= 0f)
         {
             Fire();
-            timeToFire = FireTime;
+            _timeToFire = FireTime;
         }
     }
     
     void Fire() {
         switch (Class)
         {
-            case PlayerClass.MAGE:
+            case PlayerClass.Mage:
                 FireSpell();
                 break;
         }
@@ -63,12 +110,11 @@ public class PlayerController : MonoBehaviour
 
      void FollowMouse()
      {
-        worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+        _worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
                                                               Input.mousePosition.y,
-                                                              cameraDiff));
+                                                              _cameraDiff));
 
-        transform.LookAt(new Vector3(worldPos.x, transform.position.y, worldPos.z));
-        // Seems to be only rotating 90 degrees
+        transform.LookAt(new Vector3(_worldPos.x, transform.position.y, _worldPos.z));
      }
 
     void FireSpell()
