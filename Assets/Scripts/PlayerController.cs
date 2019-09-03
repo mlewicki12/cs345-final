2dc905c435b1b971ca15dd31502e629e0f824f12
Maciej Lewicki <lewickima@drexel.edu>
2019-09-02 20:58:27 -0400

    organising + knight character beginnings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public enum PlayerClass {
-        Mage
+        Mage,
+        Knight
     }
 
-    public float Acceleration;
+    public float Speed;
     public PlayerClass Class;
     public GameObject FireSource;
     public Vector3 SpellSpeed;
 
+    public float LookRange;
     public float FireTime;
     public float ProjectileLifeTime;
     
     private float _timeToFire;
     private Vector3 _worldPos;
-    private Vector3 _speed;
     private ChatText _text;
     private Rigidbody _rigidbody;
 
@@ -28,7 +29,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _timeToFire = 0f;
-        _speed = new Vector3(0, 0, 0);
         _text = GetComponent<ChatText>();
         _rigidbody = GetComponent<Rigidbody>();
     }
@@ -38,18 +38,26 @@ public class PlayerController : MonoBehaviour
     {
         _timeToFire = Math.Max(0f, _timeToFire - Time.deltaTime);
         _rigidbody.velocity = new Vector3(0, 0, 0);
-        
-        FollowMouse();
-        
-        float moveX = Input.GetAxis("Horizontal") * Acceleration; // AD
-        float moveZ = Input.GetAxis("Vertical") * Acceleration; // WS
 
-        moveX *= Time.deltaTime;
-        moveZ *= Time.deltaTime;
+        float[] move;
+        if (Class == PlayerClass.Mage)
+        {
+            move = GetMove(KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow);
+            
+            transform.Translate(new Vector3(move[1], 0, move[0]));
+            FollowMouse();
+        }
+        else
+        {
+            move = GetMove(KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D);
+            Vector3 toPoint = new Vector3(move[1], 0, move[0]);
+            
+            transform.Translate(toPoint);
+            
+            transform.LookAt(transform.position + GetNewDir(toPoint));
+        }
 
-        _speed = new Vector3(moveX, 0, moveZ);
-        transform.Translate(_speed);
-
+            
         if (Input.GetMouseButtonDown(0))
         {
             if (_text.InConversation())
@@ -84,8 +92,12 @@ public class PlayerController : MonoBehaviour
          {
              var dir = hit.point;
              dir.y = transform.position.y;
-             
-             transform.LookAt(dir);
+
+             var comp = transform.position - dir;
+             if (comp.magnitude >= LookRange)
+             {
+                 transform.LookAt(dir);
+             }
          }
      }
 
@@ -95,5 +107,58 @@ public class PlayerController : MonoBehaviour
         spell.GetComponent<Rigidbody>().velocity = SpellSpeed.x * transform.right + SpellSpeed.z * transform.forward;
         
         Destroy(spell, ProjectileLifeTime);
+    }
+
+    float[] GetMove(KeyCode forward, KeyCode back, KeyCode left, KeyCode right)
+    {
+        var ret = new float[2];
+        ret[0] = ret[1] = 0.0f;
+
+        if (Input.GetKey(forward))
+        {
+            ret[0] += Speed;
+        }
+
+        if (Input.GetKey(back))
+        {
+            ret[0] -= Speed;
+        }
+
+        if (Input.GetKey(right))
+        {
+            ret[1] += Speed;
+        }
+
+        if (Input.GetKey(left))
+        {
+            ret[1] -= Speed;
+        }
+
+        ret[0] *= Time.deltaTime;
+        ret[1] *= Time.deltaTime;
+        
+        return ret;
+    }
+
+    Vector3 GetNewDir(Vector3 point)
+    {
+        Vector3 ret = new Vector3(0, 0, 0);
+        if (point.x < 0)
+        {
+            ret.x = 1;
+        } else if (point.x < 0)
+        {
+            ret.x = -1;
+        }
+
+        if (point.z < 0)
+        {
+            ret.z = 1;
+        } else if (point.z > 0)
+        {
+            ret.z = -1;
+        }
+
+        return ret;
     }
 }
--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
 {
-    public PlayerController.PlayerClass Class;
-    public GameObject PlayerPrefab;
+    public GameObject MagePrefab;
+    public GameObject KnightPrefab;
     
     // Start is called before the first frame update
     void Start()
     {
-        var player = Instantiate(PlayerPrefab, transform.position, transform.rotation);
-        player.GetComponent<PlayerController>().Class = Class;
-        player.name = "Player";
+        var position = transform.position;
+        var rotation = transform.rotation;
+        
+        var player = Instantiate(MagePrefab, position, rotation);
+        player.GetComponent<PlayerController>().Class = PlayerController.PlayerClass.Mage;
+        player.name = "MagePlayer";
+
+        player = Instantiate(KnightPrefab, position + new Vector3(1, 0, 0), rotation);
+        player.GetComponent<PlayerController>().Class = PlayerController.PlayerClass.Knight;
+        player.name = "KnightPlayer";
     }
 }
